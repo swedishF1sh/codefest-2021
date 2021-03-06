02bff088cb86048205da2a656fcd48b8821e761b
Weasel83 <mikerobe83@gmail.com>
2021-03-06 13:34:38 -0500

    yes

---
+++ RotationPuzzleGame/Assets/WorldRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldRotation : MonoBehaviour
+{
+
+    public Transform transform;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(1, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(-1, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Rotate(0, 0, 1);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Rotate(1, 0, -1);
+    }
+}
