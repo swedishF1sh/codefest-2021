2ddc14a8138e9a26654567653639304b44c42df5
swedishfish <pauld37509@gmail.com>
2021-03-06 22:49:38 -0500

    yes

--- RotationPuzzleGame/Assets/WorldRotation.cs
+++ RotationPuzzleGame/Assets/WorldRotation.cs
@@ -6,19 +6,25 @@ public class WorldRotation : MonoBehaviour
 {
 
     public Transform transform;
+    public float increment = 45f;
+    public Vector3 rotationVelocity;
 
    
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             transform.Rotate(1, 0, 0);
-        if (Input.GetKey(KeyCode.RightArrow))
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
             transform.Rotate(-1, 0, 0);
-        if (Input.GetKey(KeyCode.UpArrow))
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
             transform.Rotate(0, 0, 1);
-        if (Input.GetKey(KeyCode.DownArrow))
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
             transform.Rotate(0, 0, -1);
+        }
     }
 }
