00675f5859ac54688f546c52761422d7dafb4d7f
swedishfish <pauld37509@gmail.com>
2021-03-08 13:43:49 -0500

    yes
    yes

--- RotationPuzzleGame/Assets/Scripts/WorldRotation.cs
+++ RotationPuzzleGame/Assets/Scripts/WorldRotation.cs
@@ -24,10 +24,10 @@ public class WorldRotation : MonoBehaviour
             targetRotation.x -= increment;
         }
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            targetRotation.y += increment;
+            targetRotation.z += increment;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            targetRotation.y -= increment;
+            targetRotation.z -= increment;
         }
 
         if (Input.GetKeyDown(KeyCode.R)) {
