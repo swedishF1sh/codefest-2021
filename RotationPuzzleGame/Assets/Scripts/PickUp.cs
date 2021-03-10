81ce79d3d915bf7cae018334983c050e3cc53888
swedishfish <pauld37509@gmail.com>
2021-03-10 08:46:36 -0500

    added pickup range

--- RotationPuzzleGame/Assets/Scripts/PickUp.cs
+++ RotationPuzzleGame/Assets/Scripts/PickUp.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+
     public Transform destination;
+    public GameObject environment;
+    public Rigidbody rigidbody;
     public float thrust;
+    public float range = 5;
+
+    bool isPickedUp;
     
     void OnMouseDown()
     {
-        GetComponent<Rigidbody>().useGravity = false;
-        this.transform.position = destination.position;
-        this.transform.parent = GameObject.Find("Destination").transform;
+        /*
+        rigidbody.isKinematic = true;
+        rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+        */
+
+        isPickedUp = true;
+
+        if(Vector3.Distance(this.transform.position, destination.position) <= range) {
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = destination.position;
+            this.transform.parent = GameObject.Find("Destination").transform;
+        }
     }
 
     void OnMouseUp()
     {
-        this.transform.parent = null;
+        /*
+        rigidbody.isKinematic = false;
+        rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        */
+
+        isPickedUp = false;
+
+        this.transform.parent = environment.transform;
         GetComponent<Rigidbody>().useGravity = true;
         this.GetComponent<Rigidbody>().AddForce(-transform.right * thrust);
     }
+
+    
 }
