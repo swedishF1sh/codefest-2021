d171143c339e542594e523912c67d39290680b7e
Weasel83 <mikerobe83@gmail.com>
2021-03-25 15:33:41 -0400

    Puzzle 1

--- RotationPuzzleGame/Assets/Scripts/PressurePlate.cs
+++ RotationPuzzleGame/Assets/Scripts/PressurePlate.cs
@@ -6,22 +6,29 @@ public class PressurePlate : MonoBehaviour
 {
     [SerializeField]
     GameObject door;
+    [SerializeField]
+    GameObject door2;
 
     bool isOpen = false;
+   
     
     void OnTriggerEnter(Collider col)
     {
         isOpen = true;
-        if (isOpen = true)
-            door.transform.position += new Vector3(10, 0, 0);
-        
-    }
+        if (isOpen == true)
+        {
+            door.SetActive(false);
+            door2.SetActive(true);
+        }
 
-    void Update()
+    }
+    void OnTriggerExit(Collider colExit)
     {
-
-        //isOpen = false;
-        //if (isOpen == false)
-        //    door.transform.position = originalPosition;
+        door.SetActive(true);
+        door2.SetActive(false);
     }
+
+
+    
+   
 }
