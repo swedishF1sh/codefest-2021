b6897e53a5e1e95f8635ff5cdb52e58337ec5a1d
YashiDev556 <yashidev556@gmail.com>
2021-03-28 11:15:58 -0400

    Addition of Lasers and Mirrors
    Added the mechanics of mirrors and lasers, although have not implemented said mechanics into a proper puzzle

---
+++ RotationPuzzleGame/Assets/Scripts/RaycastReflection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class RaycastReflection : MonoBehaviour
+{
+    public int reflections;
+    public float maxLength;
+
+    private LineRenderer lr;
+    private Ray ray;
+    private RaycastHit hit;
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+
+    private void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+
+        lr.positionCount = 1;
+        lr.SetPosition(0, transform.position);
+        float remainingLength = maxLength;
+
+        for(int i = 0; i < reflections; i++)
+        {
+            if(Physics.Raycast(ray.origin, ray.direction, out hit, remainingLength))
+            {
+                lr.positionCount++;
+                lr.SetPosition(lr.positionCount - 1, hit.point);
+                remainingLength -= Vector3.Distance(ray.origin, hit.point);
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                if (hit.collider.tag != "Mirror")
+                    break;
+                else
+                {
+                    lr.positionCount++;
+                    lr.SetPosition(lr.positionCount - 1, ray.origin + ray.direction * remainingLength);
+                }
+            }
+        }
+    }
+}
