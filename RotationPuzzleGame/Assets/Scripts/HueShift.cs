c92aec8de5871d8a0a1b0628384cb854872fb4bf
Weasel83 <mikerobe83@gmail.com>
2021-03-29 10:08:40 -0400

    idk what this stuff is but deal with it

---
+++ RotationPuzzleGame/Assets/Scripts/HueShift.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HueShift : MonoBehaviour
+{
+    public float Speed = 1;
+    public Material material;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        material.SetColor("_Color", Color.red);
+        material.SetColor("_Color", Color.blue);
+        //material.SetColor("_Color", Color.orange);
+        material.SetColor("_Color", Color.green);
+        material.SetColor("_Color", Color.yellow);
+    }
+}
