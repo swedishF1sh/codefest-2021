ce641b7111f39be1272bfddd18d68d63f12f1156
YashiDev556 <yashidev556@gmail.com>
2021-03-31 18:53:00 -0400

    Added Main Menu
    Main Menu contains several animations, with functional play and quit buttons. There is also a loading screen that has been created, although it is quite primitive.

---
+++ RotationPuzzleGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject loadingScreen;
+
+    public string firstLevel = "SampleScene";
+
+
+    public void LoadFirst()
+    {
+        Debug.Log("LoadingScene...");
+        StartCoroutine(LoadAsync(firstLevel));
+
+    }
+
+    IEnumerator LoadAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+}
