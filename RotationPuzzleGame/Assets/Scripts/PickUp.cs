9d07680626a4283fb12bd43ae951808251e27c57
Weasel83 <mikerobe83@gmail.com>
2021-04-02 14:07:23 -0400

    failed fix for phasing objects, plz fix

--- RotationPuzzleGame/Assets/Scripts/PickUp.cs
+++ RotationPuzzleGame/Assets/Scripts/PickUp.cs
@@ -8,9 +8,12 @@ public class PickUp : MonoBehaviour
 
     public Transform destination;
     public GameObject environment;
+    public GameObject item;
     public Rigidbody rigidbody;
     public float thrust;
     public float range = 5;
+
+    public Collider envCollider, itemCollider;
 
     public bool isPickedUp;
     
@@ -25,11 +28,13 @@ public class PickUp : MonoBehaviour
         rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
         */
 
-        isPickedUp = true;
-
+        isPickedUp = false;
         if(Vector3.Distance(this.transform.position, destination.position) <= range) {
+
             GetComponent<Rigidbody>().useGravity = false;
             rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+
+
             this.transform.position = destination.position;
             this.transform.parent = GameObject.Find("Destination").transform;
         }
@@ -44,14 +49,24 @@ public class PickUp : MonoBehaviour
         rigidbody.isKinematic = false;
         rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         */
-
+        
         isPickedUp = false;
+
+        
+        //envCollider = environment.GetComponent<Collider>();
+        //itemCollider = item.GetComponent<Collider>();
+        //Vector3 closestPoint = envCollider.ClosestPointOnBounds(destination.position);
+        //float distance = Vector3.Distance(closestPoint, destination.position);
+        
         rigidbody.constraints = RigidbodyConstraints.None;
         this.transform.parent = environment.transform;
+
         GetComponent<Rigidbody>().useGravity = true;
-        
+
         this.GetComponent<Rigidbody>().AddForce(-transform.right * thrust);
+        
     }
+    
 
     
 }
